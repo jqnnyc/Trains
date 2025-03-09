177e46476b8cda0c1c10d40c0986cc3327c950e6
jqnnyc <jonathan.comfort@swlstg.nhs.uk>
2025-03-09 19:25:54 +0000

    error checking

--- trains.py
+++ trains.py
@@ -43,10 +43,12 @@ else:
 
     st.markdown(f"The next train is to the shithole of **{df.at[0,"destination"]}** at **{df.at[0, "std"]}**. It is currently due {df.at[0, "etd"]}. Hope it's your train you twat.")
 
-    st.markdown(f"After that you can piss off to **{df.at[1,"destination"]}** if you're that way inclined. That train is scheduled at **{df.at[1, "std"]}** and i'm not telling you if it's on time because i think you're a prick.")
+    if len(df) > 1:
+        st.markdown(f"After that you can piss off to **{df.at[1,"destination"]}** if you're that way inclined. That train is scheduled at **{df.at[1, "std"]}** and i'm not telling you if it's on time because i think you're a prick.")
+    else:
+        st.error("After that, there are no more fucking trains in the next couple of hours. Bad luck bellend!")
 
-    st.markdown("Oh you want more? Well here's the fucking departure board:")
-
+    st.markdown("Here's the fucking departure board:")
     t = df[['std','etd','destination','platform']]
     st.table(t)
 
